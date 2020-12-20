a6379a0d949ce1e922ca6411f154b2acc6b3af7f
Leo <lapyaeootharthar@gmail.com>
2020-12-21 00:39:36 +0630

    Fix: Register page UI

--- lib/modules/register/components/register_body.dart
+++ lib/modules/register/components/register_body.dart
@@ -9,42 +9,47 @@ class RegisterBody extends StatelessWidget {
     return SafeArea(
         child: SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [
-          RegisterHeader(),
-          SizedBox(
-            height: getScreenWidthRation(10.0),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: getScreenWidthRation(20.0)),
-            child: Column(
-              children: [
-                Text(
-                  'Register Account',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                  ),
-                ),
-                SizedBox(
-                  height: getScreenHeightRation(10.0),
-                ),
-                Text(
-                  'Complete your detail or \n continue with social media',
-                  textAlign: TextAlign.center,
-                ),
-                RegisterForm(),
-                SizedBox(
-                  height: getScreenHeightRation(20.0),
-                ),
-                Text(
-                  'By continuing your confirm that you agree\n with our Term and Contidion',
-                  textAlign: TextAlign.center,
-                ),
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: getScreenHeightRation(10.0),
             ),
-          )
-        ],
+            RegisterHeader(),
+            SizedBox(
+              height: getScreenWidthRation(10.0),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: getScreenWidthRation(20.0)),
+              child: Column(
+                children: [
+                  Text(
+                    'Register Account',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: getScreenHeightRation(10.0),
+                  ),
+                  Text(
+                    'Complete your detail or \n continue with social media',
+                    textAlign: TextAlign.center,
+                  ),
+                  RegisterForm(),
+                  SizedBox(
+                    height: getScreenHeightRation(20.0),
+                  ),
+                  Text(
+                    'By continuing your confirm that you agree\n with our Term and Contidion',
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
